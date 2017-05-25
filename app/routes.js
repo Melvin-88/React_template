11fb395baae328011ba151eea69a23e0a82b46dd
Melvin-88 <taisen747@gmail.com>
2017-05-25 16:44:37 +0300

    add component

---
+++ app/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IndexRoute, Route, Link } from 'react-router'
+import { render } from 'react-dom'
+
+import App from './components/app';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+
+import Home from "./components/home/Home";
+
+export default (
+    <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/contact" component={Contact}/>
+        <Route path="/*" component={Home}/>
+    </Route>
+);
